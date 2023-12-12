755c5b756913d95cf8e653e01a26f7a4e25e5120
mrshigi <sraudat@email.sc.edu>
2023-12-12 18:47:39 -0500

    testin

---
+++ public/script1.js
@@ -0,0 +1,218 @@
+const getBooks = async() => {
+    try {
+        return (await fetch("https://a16.onrender.com/api/books")).json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+const addEditBook = async (e) => {
+    e.preventDefault();
+    const form = document.getElementById("add-edit-book-form");
+    const formData = new FormData(form);
+
+    if (form._id.value) { // Edit book
+        await fetch(`/api/books/${form._id.value}`, {
+            method: "PUT",
+            body: formData
+        });
+    } else { // Add new book
+        await fetch("/api/books", {
+            method: "POST",
+            body: formData
+        });
+    }
+
+    resetForm();
+    document.querySelector(".dialog").classList.add("transparent");
+    showBooks();
+};
+
+// Function to show a confirmation prompt before deletion
+const confirmDelete = async (bookId) => {
+    if (confirm("Are you sure you want to delete this book?")) {
+        await fetch(`/api/books/${bookId}`, {
+            method: "DELETE"
+        });
+        showBooks();
+    }
+};
+
+
+const showBooks = async() => {
+    let books = await getBooks();
+    let booksDiv = document.getElementById("book-list");
+    booksDiv.innerHTML = "";
+    books.forEach((book) => {
+        const section = document.createElement("section");
+        section.classList.add("book");
+        booksDiv.append(section);
+
+        let img = document.createElement("img");
+        section.append(img);
+        img.src="https://a16.onrender.com/" + book.img;
+
+        const a = document.createElement("a");
+        a.href = "#";
+        section.append(a);
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = book.name;
+        a.append(h3);
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.onclick = () => confirmDelete(book._id);
+        section.appendChild(deleteButton);
+
+        a.onclick = (e) => {
+            e.preventDefault();
+            displayDetails(book);
+        };
+    });
+};
+
+
+const displayDetails = (book) => {
+    const bookDetails = document.getElementById("book-details");
+    bookDetails.innerHTML = "";
+
+    const h3 = document.createElement("h3");
+    h3.innerHTML = book.name;
+    bookDetails.append(h3);
+
+    const dLink = document.createElement("a");
+    dLink.innerHTML = "	&#x2715;";
+    bookDetails.append(dLink);
+    dLink.id = "delete-link";
+
+    const eLink = document.createElement("a");
+    eLink.innerHTML = "&#9998;";
+    bookDetails.append(eLink);
+    eLink.id = "edit-link";
+
+    const p = document.createElement("p");
+    bookDetails.append(p);
+    p.innerHTML = book.description;
+
+    const ul = document.createElement("ul");
+    bookDetails.append(ul);
+    console.log(book.summary);
+    book.summaries.forEach((summary) => {
+        const li = document.createElement("li");
+        ul.append(li);
+        li.innerHTML = summary;
+    });
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.onclick = () => confirmDelete(book._id);
+    bookDetails.append(deleteButton);
+
+    eLink.onclick = (e) => {
+        e.preventDefault();
+        document.querySelector(".dialog").classList.remove("transparent");
+        document.getElementById("add-edit-title").innerHTML = "Edit Books";
+    };
+
+    dLink.onclick = (e) => {
+        e.preventDefault();
+    };
+
+    populateEditForm(book);
+};
+const populateEditForm = (book) => {
+    document.getElementById("name").value = book.name;
+    document.getElementById("description").value = book.description;
+    // Populate other fields as necessary
+    document.getElementById("book-id").value = book._id;
+};
+
+const getSummaries = () => {
+    const inputs = document.querySelectorAll("#summary-boxes input");
+    let summaries = [];
+
+    inputs.forEach((input) => {
+        summaries.push(input.value);
+    });
+
+    return summaries;
+}
+
+const resetForm = () => {
+    const form = document.getElementById("add-edit-book-form");
+    form.reset();
+    form._id = "-1";
+    document.getElementById("summary-boxes").innerHTML = "";
+};
+
+const showHideAdd = (e) => {
+    e.preventDefault();
+    document.querySelector(".dialog").classList.remove("transparent");
+    document.getElementById("add-edit-title").innerHTML = "Add Book";
+    resetForm();
+};
+
+const addBook = (e) => {
+    e.preventDefault();
+    const section = document.getElementById("summary-boxes");
+    const input = document.createElement("input");
+    input.type = "text";
+    section.append(input);
+}
+function showEditForm(bookId) {
+    const book = document.getElementById(bookId);
+    const title = book.querySelector('h3').textContent;
+    const description = book.querySelector('p').textContent;
+
+    document.getElementById('edit-id').value = bookId;
+    document.getElementById('edit-title').value = title;
+    document.getElementById('edit-description').value = description;
+
+    document.getElementById('edit-form-container').style.display = 'block';
+}
+
+function hideEditForm() {
+    document.getElementById('edit-form-container').style.display = 'none';
+}
+
+document.getElementById('edit-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const id = document.getElementById('edit-id').value;
+    const updatedTitle = document.getElementById('edit-title').value;
+    const updatedDescription = document.getElementById('edit-description').value;
+
+    // Send PUT request to server
+    fetch(`/api/books/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            title: updatedTitle,
+            description: updatedDescription
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Update the item in the view
+        const book = document.getElementById(id);
+        book.querySelector('h3').textContent = updatedTitle;
+        book.querySelector('p').textContent = updatedDescription;
+
+        hideEditForm();
+    })
+    .catch(error => console.error('Error:', error));
+});
+
+
+window.onload = () => {
+    showBooks();
+    document.getElementById("add-edit-book-form").onsubmit = addEditBook;
+    document.getElementById("add-link").onclick = showHideAdd;
+
+    document.querySelector(".close").onclick = () => {
+        document.querySelector(".dialog").classList.add("transparent");
+    };
+
+    document.getElementById("add-summary").onclick = addBook;
+};
---
+++ server1.js
@@ -0,0 +1,141 @@
+const express = require("express");
+const app = express();
+const Joi = require("joi");
+const multer = require("multer");
+app.use(express.static("public"));
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+const upload = multer({ dest: __dirname + "/public/images" });
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+let books = [{
+    _id: 1,
+    name: "The Road",
+    description: "Apocolyptic",
+    rating: 4,
+    summaries: [
+        "Basic Overview: A man and his son's journey",
+        "Nuclear winter event happened",
+        "Man's wife kills herself",
+        "Man left alone with child",
+        "They fight to survive daily",
+        "Run from human eating folk",
+        "Man is sick",
+        "Man brings child to coast",
+        "Man get shot in the leg by arrow",
+        "Man dies",
+        "Family finds child, saves him",
+        "The end, super depressing read",
+    ],
+    img: "images/road.png",
+},
+{
+    _id: 2,
+    name: "Neon Genesis: Evangelion",
+    description: "Mecha Manga",
+    rating: 5,
+    summaries: [
+        "Basic Overview: Shinji Ikaris journey to self acceptance, via ending the world",
+        "Get in the Eva Shinji",
+        "Angels from 'god' sent to earth",
+        "Shinji beats up some angels",
+        "Sinister plan in the background",
+        "2 other girls pilot the evas",
+        "Shinji get in the F*ing eva",
+        "World Govt steps in to stop sinister plan",
+        "Stuff hits the fan and Shinji kinda brings end of world",
+        "Shinji accepts himself, stops total world destruction but resets world population",
+    ],
+    img: "images/eva.png",
+},
+{
+    _id: 3,
+    name: "Options Pricing and Volatility",
+    description: "Finance",
+    rating:5,
+    summaries: [
+        "Basic Overview: The stock market wasnt enough of a way to lose money",
+        "Introduce Forwards",
+        "Introduce Future contracts",
+        "Introduce options",
+        "Introduce the Greeks, these are first and secord order Derivs of Stock Price, Volatility and Time",
+        "Math brainf*ck: no joke Stochastic Processes and partial derivatives on each Greek",
+        "Basic Options strategies: Straddle, Strangle, Covered Call, Protected Put",
+        "Wizard Options strategies: Calendar Spreads, Iron Condor, Christmas Trees, literally sounds fake",
+        "COMBINE THEM ALL INTO COMPLEX FINACIAL INSTRUMENTS",
+        "BLOW UP WORLD ECONOMY VIA DEBT DERIVATIVES",
+    ],
+    img: "images/options.png",
+},
+];
+
+app.get("/api/books", (req, res) => {
+res.send(books);
+});
+
+app.post("/api/books", upload.single("img"), (req, res) => {
+const result = validateBook(req.body);
+
+if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+}
+app.put("/api/books/:id", upload.single("img"), (req, res) => {
+    const { id } = req.params;
+    let bookFound = false;
+    
+    books = books.map(book => {
+        if (book._id == id) {
+            bookFound = true;
+            return {
+                ...book,
+                name: req.body.name,
+                description: req.body.description
+                // Update other fields as necessary
+            };
+        }
+        return book;
+    });
+
+    if (bookFound) {
+        res.send(books);
+    } else {
+        res.status(404).send("Book not found");
+    }
+});
+app.delete("/api/books/:id", (req, res) => {
+    const { id } = req.params;
+    books = books.filter(book => book._id != id);
+    res.send(books);
+});
+
+const book = {
+    _id: books.length + 1,
+    name: req.body.name,
+    description: req.body.description,
+    summaries: req.body.summaries.split(",")
+}
+
+books.push(book);
+res.send(books);
+});
+
+const validateBook = (book) => {
+    const schema = Joi.object({
+        _id: Joi.allow(""),
+        summaries: Joi.allow(""),
+        name: Joi.string().min(3).required(),
+        description: Joi.string().min(3).required()
+    });
+
+    return schema.validate(book);
+};
+
+app.listen(3000, () => {
+console.log("666 satan mf");
+});
