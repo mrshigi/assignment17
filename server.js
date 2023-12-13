ec3454764ea02a43ae5b55c4e53b2d37e3ed76d6
mrshigi <sraudat@email.sc.edu>
2023-12-13 18:23:05 -0500

    trying rewrote entire server file

--- server.js
+++ server.js
@@ -3,170 +3,110 @@ const app = express();
 const Joi = require("joi");
 const multer = require("multer");
 const mongoose = require("mongoose");
+const path = require("path");
+
 app.use(express.static("public"));
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
-
+// MongoDB connection
 mongoose
-  .connect("mongodb+srv://sraudat:seaner@cluster0.oh5cmuh.mongodb.net/?retryWrites=true&w=majority")
-  .then(() => console.log("Connected to mongodb..."))
-  .catch((err) => console.error("could not connect ot mongodb...", err));
+  .connect("mongodb+srv://sraudat:seaner@cluster0.oh5cmuh.mongodb.net/")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
-  const bookSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    rating: Number,
-    summaries: [String],
-    img: String // Store the image path
-  });
-
-  const Book = mongoose.model('Book', bookSchema);
-
-// Configure Multer for image uploads
+// Multer configuration for image upload
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'public/uploads/');
+  destination: function (req, file, cb) {
+    cb(null, "public/images");
   },
-  filename: function(req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+  },
 });
-
 const upload = multer({ storage: storage });
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+
+// Mongoose schema and model for books
+const bookSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  rating: Number,
+  summaries: [String],
+  img: String, // For storing image path
 });
 
-let books = [
-  {
-    _id: 1,
-    name: "The Road",
-    description: "Apocolyptic",
-    rating: 4,
-    summaries: [
-      "Basic Overview: A man and his son's journey",
-      "Nuclear winter event happened",
-      "Man's wife kills herself",
-      "Man left alone with child",
-      "They fight to survive daily",
-      "Run from human eating folk",
-      "Man is sick",
-      "Man brings child to coast",
-      "Man get shot in the leg by arrow",
-      "Man dies",
-      "Family finds child, saves him",
-      "The end, super depressing read",
-    ],
-    img: "images/road.png",
-  },
-  {
-    _id: 2,
-    name: "Neon Genesis: Evangelion",
-    description: "Mecha Manga",
-    rating: 5,
-    summaries: [
-      "Basic Overview: Shinji Ikaris journey to self acceptance, via ending the world",
-      "Get in the Eva Shinji",
-      "Angels from 'god' sent to earth",
-      "Shinji beats up some angels",
-      "Sinister plan in the background",
-      "2 other girls pilot the evas",
-      "Shinji get in the F*ing eva",
-      "World Govt steps in to stop sinister plan",
-      "Stuff hits the fan and Shinji kinda brings end of world",
-      "Shinji accepts himself, stops total world destruction but resets world population",
-    ],
-    img: "images/eva.png",
-  },
-  {
-    _id: 3,
-    name: "Options Pricing and Volatility",
-    description: "Finance",
-    rating: 5,
-    summaries: [
-      "Basic Overview: The stock market wasnt enough of a way to lose money",
-      "Introduce Forwards",
-      "Introduce Future contracts",
-      "Introduce options",
-      "Introduce the Greeks, these are first and secord order Derivs of Stock Price, Volatility and Time",
-      "Math brainf*ck: no joke Stochastic Processes and partial derivatives on each Greek",
-      "Basic Options strategies: Straddle, Strangle, Covered Call, Protected Put",
-      "Wizard Options strategies: Calendar Spreads, Iron Condor, Christmas Trees, literally sounds fake",
-      "COMBINE THEM ALL INTO COMPLEX FINACIAL INSTRUMENTS",
-      "BLOW UP WORLD ECONOMY VIA DEBT DERIVATIVES",
-    ],
-    img: "images/options.png",
-  },
-];
+const Book = mongoose.model("Book", bookSchema);
 
-app.get("/api/books", (req, res) => {
-  res.send(books);
-});
-app.post('/api/books', upload.single('img'), async (req, res) => {
-  // Validate data with Joi
-  const { error } = bookValidationSchema.validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  // Create a new book instance
-  const book = new Book({
-    name: req.body.name,
-    description: req.body.description,
-    rating: req.body.rating,
-    summaries: req.body.summaries.split(','),
-    img: req.file.path
-  });
-
-  try {
-    await book.save();
-    res.send(book);
-  } catch (err) {
-    res.status(500).send('Error saving book');
-  }
-});
-app.put('/api/books/:id', upload.single('img'), async (req, res) => {
-  // Validate data with Joi
-  const { error } = bookValidationSchema.validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  try {
-    const book = await Book.findByIdAndUpdate(req.params.id, {
-      name: req.body.name,
-      description: req.body.description,
-      rating: req.body.rating,
-      summaries: req.body.summaries.split(','),
-      img: req.file ? req.file.path : req.body.img
-    }, { new: true });
-
-    if (!book) return res.status(404).send('Book not found');
-    res.send(book);
-  } catch (err) {
-    res.status(500).send('Error updating book');
-  }
-});
-
-app.delete('/api/books/:id', async (req, res) => {
-  try {
-    const book = await Book.findByIdAndRemove(req.params.id);
-    if (!book) return res.status(404).send('Book not found');
-    res.send(book);
-  } catch (err) {
-    res.status(500).send('Error deleting book');
-  }
-});
+// Joi schema for validation
 const validateBook = (book) => {
   const schema = Joi.object({
-    _id: Joi.allow(""),
-    summaries: Joi.allow(""),
     name: Joi.string().min(3).required(),
     description: Joi.string().min(3).required(),
-    img: Joi.string().allow("")
+    rating: Joi.number().min(1).max(5).optional(),
+    summaries: Joi.array().items(Joi.string()).optional(),
+    img: Joi.string().optional(),
   });
 
   return schema.validate(book);
 };
 
+// GET endpoint to serve index.html
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// GET endpoint to retrieve all books
+app.get("/api/books", async (req, res) => {
+  const books = await Book.find();
+  res.send(books);
+});
+
+// POST endpoint to add a new book
+app.post("/api/books", upload.single("img"), async (req, res) => {
+  const { error } = validateBook(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const book = new Book({
+    name: req.body.name,
+    description: req.body.description,
+    rating: req.body.rating,
+    summaries: req.body.summaries.split(","),
+    img: req.file ? req.file.path : "",
+  });
+
+  await book.save();
+  res.send(book);
+});
+
+// PUT endpoint to update a book
+app.put("/api/books/:id", upload.single("img"), async (req, res) => {
+  const { error } = validateBook(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const book = await Book.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      rating: req.body.rating,
+      summaries: req.body.summaries.split(","),
+      img: req.file ? req.file.path : req.body.img,
+    },
+    { new: true }
+  );
+
+  if (!book) return res.status(404).send("Book not found");
+  res.send(book);
+});
+
+// DELETE endpoint to delete a book
+app.delete("/api/books/:id", async (req, res) => {
+  const book = await Book.findByIdAndRemove(req.params.id);
+  if (!book) return res.status(404).send("Book not found");
+  res.send("Book deleted");
+});
+
 app.listen(3000, () => {
-  console.log("666 satan mf");
+  console.log("Server started on port 3000");
 });
