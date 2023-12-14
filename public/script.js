7b55568c2c7a2effd2f4f7500550a242d3f48df2
mrshigi <sraudat@email.sc.edu>
2023-12-13 22:21:16 -0500

    wokr

--- public/script.js
+++ public/script.js
@@ -18,7 +18,7 @@ const showBooks = async () => {
 
     const books = await response.json();
 
-    const booksContainer = document.getElementById("books-container");
+    const booksContainer = document.getElementById("books-list");
     if (booksContainer) {
       booksContainer.innerHTML = "";
     } else {
@@ -52,110 +52,6 @@ const showBooks = async () => {
     console.error("Error fetching books:", error);
   }
 };
-async function saveEditedBook(formData) {
-  const response = await fetch("/api/books/" + formData.get("_id"), {
-    method: "PUT",
-    body: formData,
-  });
-  if (response.status === 200) {
-    // Update view
-    showBooks();
-  } else {
-    // Handle error
-    console.error("Error updating book");
-  }
-}
-
-async function deleteBook(bookId) {
-  if (confirm("Are you sure you want to delete this book?")) {
-    const response = await fetch("/api/books/" + bookId, {
-      method: "DELETE",
-    });
-    if (response.status === 200) {
-      // Update view
-      showBooks();
-    } else {
-      // Handle error
-      console.error("Error deleting book");
-    }
-  }
-}
-
-const displayDetails = (book) => {
-  const bookDetails = document.getElementById("book-details");
-  bookDetails.innerHTML = "";
-
-  const h3 = document.createElement("h3");
-  h3.innerHTML = book.name;
-  bookDetails.append(h3);
-
-  const dLink = document.createElement("a");
-  dLink.innerHTML = "	&#x2715;";
-  bookDetails.append(dLink);
-  dLink.id = "delete-link";
-
-  const eLink = document.createElement("a");
-  eLink.innerHTML = "&#9998;";
-  bookDetails.append(eLink);
-  eLink.id = "edit-link";
-
-  const p = document.createElement("p");
-  bookDetails.append(p);
-  p.innerHTML = book.description;
-
-  const ul = document.createElement("ul");
-  bookDetails.append(ul);
-  console.log(book.summary);
-  book.summaries.forEach((summary) => {
-    const li = document.createElement("li");
-    ul.append(li);
-    li.innerHTML = summary;
-  });
-
-  eLink.onclick = (e) => {
-    e.preventDefault();
-    document.querySelector(".dialog").classList.remove("transparent");
-    document.getElementById("add-edit-title").innerHTML = "Edit Books";
-  };
-
-  dLink.onclick = (e) => {
-    e.preventDefault();
-  };
-
-  populateEditForm(book);
-};
-
-//document.getElementById('add-edit-book-container').addEventListener('submit', handleEditFormSubmit);
-
-const populateEditForm = (book) => {
-  const form = document.getElementById("add-edit-book-form");
-  form._id.value = book._id;
-  document.getElementById("name").value = book.name;
-  document.getElementById("description").value = book.description;
-  document.getElementById("summaries").value = book.summaries.join(", ");
-  document.getElementById("add-edit-title").textContent = "Edit Book";
-  document.querySelector(".dialog").classList.remove("transparent");
-};
-
-const handleEditFormSubmit = async (event) => {
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  formData.append("summaries", getSummaries().join(","));
-
-  try {
-    const response = await fetch("/api/books/" + formData.get("_id"), {
-      method: "PUT",
-      body: formData,
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    await showBooks(); // Refresh the book list
-  } catch (error) {
-    console.error("Error updating book:", error);
-  }
-};
-
 const addEditBook = async (e) => {
   e.preventDefault();
   const form = document.getElementById("add-edit-book-form");
@@ -204,6 +100,112 @@ const addEditBook = async (e) => {
     console.error("Error submitting form:", error);
   }
 };
+const displayDetails = (book) => {
+  const bookDetails = document.getElementById("book-details");
+  bookDetails.innerHTML = "";
+
+  const h3 = document.createElement("h3");
+  h3.innerHTML = book.name;
+  bookDetails.append(h3);
+
+  const dLink = document.createElement("a");
+  dLink.innerHTML = "	&#x2715;";
+  bookDetails.append(dLink);
+  dLink.id = "delete-link";
+
+  const eLink = document.createElement("a");
+  eLink.innerHTML = "&#9998;";
+  bookDetails.append(eLink);
+  eLink.id = "edit-link";
+
+  const p = document.createElement("p");
+  bookDetails.append(p);
+  p.innerHTML = book.description;
+
+  const ul = document.createElement("ul");
+  bookDetails.append(ul);
+  console.log(book.summary);
+  book.summaries.forEach((summary) => {
+    const li = document.createElement("li");
+    ul.append(li);
+    li.innerHTML = summary;
+  });
+
+  eLink.onclick = (e) => {
+    e.preventDefault();
+    document.querySelector(".dialog").classList.remove("transparent");
+    document.getElementById("add-edit-title").innerHTML = "Edit Books";
+  };
+
+  dLink.onclick = (e) => {
+    e.preventDefault();
+  };
+
+  populateEditForm(book);
+};
+async function saveEditedBook(formData) {
+  const response = await fetch("/api/books/" + formData.get("_id"), {
+    method: "PUT",
+    body: formData,
+  });
+  if (response.status === 200) {
+    // Update view
+    showBooks();
+  } else {
+    // Handle error
+    console.error("Error updating book");
+  }
+}
+
+async function deleteBook(bookId) {
+  if (confirm("Are you sure you want to delete this book?")) {
+    const response = await fetch("/api/books/" + bookId, {
+      method: "DELETE",
+    });
+    if (response.status === 200) {
+      // Update view
+      showBooks();
+    } else {
+      // Handle error
+      console.error("Error deleting book");
+    }
+  }
+}
+
+
+
+//document.getElementById('add-edit-book-container').addEventListener('submit', handleEditFormSubmit);
+
+const populateEditForm = (book) => {
+  const form = document.getElementById("add-edit-book-form");
+  form._id.value = book._id;
+  document.getElementById("name").value = book.name;
+  document.getElementById("description").value = book.description;
+  document.getElementById("summaries").value = book.summaries.join(", ");
+  document.getElementById("add-edit-title").textContent = "Edit Book";
+  document.querySelector(".dialog").classList.remove("transparent");
+};
+
+const handleEditFormSubmit = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  formData.append("summaries", getSummaries().join(","));
+
+  try {
+    const response = await fetch("/api/books/" + formData.get("_id"), {
+      method: "PUT",
+      body: formData,
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    await showBooks(); // Refresh the book list
+  } catch (error) {
+    console.error("Error updating book:", error);
+  }
+};
+
+
 function displayError(errorMessage) {
   const errorElement = document.getElementById("error-message");
   if (errorElement) {
