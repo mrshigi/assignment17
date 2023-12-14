b0574ece9e2f39994718c534a8bd449e06ac3c3a
mrshigi <sraudat@email.sc.edu>
2023-12-14 01:50:45 -0500

    im done

--- server.js
+++ server.js
@@ -24,12 +24,82 @@ app.get("/", (req, res) => {
 });
 const upload = multer({ dest: __dirname + "/public/images" });
 
+
 const bookschema = new mongoose.Schema({
   name: String,
   description: String,
   summaries: [String],
   img: String, // For storing image path
 });
+const books = [
+  {
+    _id: 1,
+    name: "The Road",
+    description: "Apocolyptic",
+    summaries: [
+      "Basic Overview: A man and his son's journey",
+      "Nuclear winter event happened",
+      "Man's wife kills herself",
+      "Man left alone with child",
+      "They fight to survive daily",
+      "Run from human eating folk",
+      "Man is sick",
+      "Man brings child to coast",
+      "Man get shot in the leg by arrow",
+      "Man dies",
+      "Family finds child, saves him",
+      "The end, super depressing read",
+    ],
+    img: "images/road.png",
+  },
+  {
+    _id: 2,
+    name: "Neon Genesis: Evangelion",
+    description: "Mecha Manga",
+    summaries: [
+      "Basic Overview: Shinji Ikaris journey to self acceptance, via ending the world",
+      "Get in the Eva Shinji",
+      "Angels from 'god' sent to earth",
+      "Shinji beats up some angels",
+      "Sinister plan in the background",
+      "2 other girls pilot the evas",
+      "Shinji get in the F*ing eva",
+      "World Govt steps in to stop sinister plan",
+      "Stuff hits the fan and Shinji kinda brings end of world",
+      "Shinji accepts himself, stops total world destruction but resets world population",
+    ],
+    img: "images/eva.png",
+  },
+  {
+    _id: 3,
+    name: "Options Pricing and Volatility",
+    description: "Finance",
+    summaries: [
+      "Basic Overview: The stock market wasnt enough of a way to lose money",
+      "Introduce Forwards",
+      "Introduce Future contracts",
+      "Introduce options",
+      "Introduce the Greeks, these are first and secord order Derivs of Stock Price, Volatility and Time",
+      "Math brainf*ck: no joke Stochastic Processes and partial derivatives on each Greek",
+      "Basic Options strategies: Straddle, Strangle, Covered Call, Protected Put",
+      "Wizard Options strategies: Calendar Spreads, Iron Condor, Christmas Trees, literally sounds fake",
+      "COMBINE THEM ALL INTO COMPLEX FINACIAL INSTRUMENTS",
+      "BLOW UP WORLD ECONOMY VIA DEBT DERIVATIVES",
+    ],
+    img: "images/options.png",
+  },
+];
+async function seedBooks() {
+  for (const bookData of books) {
+    const book = new Book(bookData);
+    await book.save();
+  }
+
+  console.log("Books have been seeded!");
+  db.close();
+}
+
+seedBooks();
 
 const Book = mongoose.model("Book", bookschema);
 
@@ -106,7 +176,6 @@ const removeBook = async (res, id) => {
 
 const validateBook = (book) => {
   const schema = Joi.object({
-    _id: Joi.allow(""),
     name: Joi.string().min(3).required(),
     description: Joi.string().min(3).required(),
     summaries: Joi.array().items(Joi.string()).optional(),
