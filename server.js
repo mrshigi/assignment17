b0462113acbda304b552ea40a93c7cc0cc5798b6
mrshigi <sraudat@email.sc.edu>
2023-12-14 01:53:07 -0500

    im done

--- server.js
+++ server.js
@@ -89,17 +89,7 @@ const books = [
     img: "images/options.png",
   },
 ];
-async function seedBooks() {
-  for (const bookData of books) {
-    const book = new Book(bookData);
-    await book.save();
-  }
 
-  console.log("Books have been seeded!");
-  db.close();
-}
-
-seedBooks();
 
 const Book = mongoose.model("Book", bookschema);
 
@@ -173,6 +163,17 @@ const removeBook = async (res, id) => {
   const book = await Book.findByIdAndDelete(id);
   res.send(book);
 };
+async function seedBooks() {
+  for (const bookData of books) {
+    const book = new Book(bookData);
+    await book.save();
+  }
+
+  console.log("Books have been seeded!");
+  db.close();
+}
+
+seedBooks();
 
 const validateBook = (book) => {
   const schema = Joi.object({
