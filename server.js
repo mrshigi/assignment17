73671f07c596e102017097b290c2c47f69c7ede3
mrshigi <sraudat@email.sc.edu>
2023-12-14 01:08:00 -0500

    mongo links please work

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 // MongoDB connection
 mongoose
   .connect(
-    "mongodb+srv://sraudat:seaner@data.fx1dsw5.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://sraudat:seaner@data.fx1dsw5.mongodb.net/"
   )
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
